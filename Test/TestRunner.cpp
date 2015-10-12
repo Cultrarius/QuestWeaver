627295da29ffd49cf83bbfd77698dafdd396d77a
Michael Galetzka <galetzka@posteo.de>
2015-10-13 00:15:39 +0200

    Build fix again...

--- Test/TestRunner.cpp
+++ Test/TestRunner.cpp
@@ -6,4 +6,6 @@
 #pragma ide diagnostic ignored "UnusedImportStatement"
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#include "catch.hpp"
+
 #pragma clang diagnostic pop
