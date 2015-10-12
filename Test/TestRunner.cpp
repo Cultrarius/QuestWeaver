b612194acc00579ab9da2ca92cde982fbd4c7624
Michael Galetzka <galetzka@posteo.de>
2015-10-13 00:15:17 +0200

    Travis file fix

--- Test/TestRunner.cpp
+++ Test/TestRunner.cpp
@@ -2,5 +2,8 @@
 // Created by michael on 12.10.15.
 //
 
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnusedImportStatement"
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
+#pragma clang diagnostic pop
