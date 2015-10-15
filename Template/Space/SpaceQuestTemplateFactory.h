4c16e56255ad2b252d31dafddbf909eba0139cf3
Michael Galetzka <galetzka@posteo.de>
2015-10-15 08:52:56 +0200

    New tests added

--- Template/Space/SpaceQuestTemplateFactory.h
+++ Template/Space/SpaceQuestTemplateFactory.h
@@ -11,6 +11,7 @@ namespace weave {
     public:
         explicit SpaceQuestTemplateFactory(std::shared_ptr<RandomStream> randomStream);
 
+    protected:
         std::shared_ptr<Template> createFromJsonValues(const Json::Value &root) const;
     };
 }
--- Test/Space/TemplateTest.cpp
+++ Test/Space/TemplateTest.cpp
@@ -7,6 +7,7 @@
 #include "../catch.hpp"
 #include "../../Core/WeaverUtils.h"
 #include "../../Template/Space/SpaceQuestTemplateFactory.h"
+#include "../../World/Space/SpaceWorldModel.h"
 
 using namespace weave;
 using namespace std;
@@ -16,8 +17,7 @@ TEST_CASE("Template factory", "[template]") {
     SpaceQuestTemplateFactory factory(rs);
     REQUIRE(factory.GetTemplateKeys().size() >= 1);
 
-
-    SECTION("checking the keys") {
+    SECTION("Checking the keys") {
         vector<string> keys = {"ExploreRegionQuest"};
         for (string key : keys) {
             bool hasKey = false;
@@ -35,6 +35,57 @@ TEST_CASE("Template factory", "[template]") {
         for (string templateKey : factory.GetTemplateKeys()) {
             auto temp = factory.CreateTemplate(templateKey);
             REQUIRE(temp.get() != nullptr);
+        }
+    }
+}
+
+TEST_CASE("Templates", "[template]") {
+    shared_ptr<RandomStream> rs = make_shared<RandomStream>(42);
+    SpaceQuestTemplateFactory factory(rs);
+
+    SECTION("Checking properties size") {
+        for (string templateKey : factory.GetTemplateKeys()) {
+            auto tp = factory.CreateTemplate(templateKey);
+            REQUIRE(tp->GetProperties().size() > 0);
+        }
+    }
+
+    SECTION("Checking mandatory property exists") {
+        for (string templateKey : factory.GetTemplateKeys()) {
+            auto tp = factory.CreateTemplate(templateKey);
+            bool hasMandatoryProperty = false;
+            for (auto property : tp->GetProperties()) {
+                if (property.IsMandatory()) {
+                    hasMandatoryProperty = true;
+                    break;
+                }
+            }
+            INFO(templateKey);
+            REQUIRE(hasMandatoryProperty);
+        }
+    }
+
+    SECTION("Checking property names") {
+        for (string templateKey : factory.GetTemplateKeys()) {
+            auto tp = factory.CreateTemplate(templateKey);
+            bool hasMandatoryProperty = false;
+            for (auto property : tp->GetProperties()) {
+                INFO(property.GetName());
+                REQUIRE(!property.GetName().empty());
+            }
+        }
+    }
+
+    SECTION("Checking property candidates") {
+        WorldModel *worldModel = new SpaceWorldModel(rs);
+        for (string templateKey : factory.GetTemplateKeys()) {
+            auto tp = factory.CreateTemplate(templateKey);
+            bool hasMandatoryProperty = false;
+            for (auto property : tp->GetProperties()) {
+                auto candidates = tp->GetPropertyCandidates(property, *worldModel);
+                INFO(templateKey + " / " + property.GetName());
+                REQUIRE(candidates.size() > 0);
+            }
         }
     }
 }
