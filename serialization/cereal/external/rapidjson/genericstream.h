18e203bec538a644ef7a125bad2d18b842ed29f3
Michael Galetzka <galetzka@posteo.de>
2015-10-18 11:47:22 +0200

    reformatted

--- serialization/cereal/external/rapidjson/genericstream.h
+++ serialization/cereal/external/rapidjson/genericstream.h
@@ -6,100 +6,118 @@
 #include <iostream>
 
 #ifdef _MSC_VER
-  #pragma warning(push)
-  #pragma warning(disable: 4127) // conditional expression is constant
-  #pragma warning(disable: 4512) // assignment operator could not be generated
-  #pragma warning(disable: 4100) // unreferenced formal parameter
+#pragma warning(push)
+#pragma warning(disable: 4127) // conditional expression is constant
+#pragma warning(disable: 4512) // assignment operator could not be generated
+#pragma warning(disable: 4100) // unreferenced formal parameter
 #endif
 
 namespace rapidjson {
 
-  //! Wrapper of std::istream for input.
-  class GenericReadStream {
+    //! Wrapper of std::istream for input.
+    class GenericReadStream {
     public:
-      typedef char Ch;    //!< Character type (byte).
+        typedef char Ch;    //!< Character type (byte).
 
-      //! Constructor.
-      /*!
-        \param is Input stream.
-        */
-      GenericReadStream(std::istream & is) : is_(&is) {
-      }
-
-
-      Ch Peek() const {
-        if(is_->eof()) return '\0';
-        return static_cast<char>(is_->peek());
-      }
-
-      Ch Take() {
-        if(is_->eof()) return '\0';
-        return static_cast<char>(is_->get());
-      }
-
-      size_t Tell() const {
-        return (int)is_->tellg();
-      }
-
-      // Not implemented
-      void Put(Ch)       { RAPIDJSON_ASSERT(false); }
-      void Flush()       { RAPIDJSON_ASSERT(false); }
-      Ch* PutBegin()     { RAPIDJSON_ASSERT(false); return 0; }
-      size_t PutEnd(Ch*) { RAPIDJSON_ASSERT(false); return 0; }
-
-      std::istream * is_;
-  };
-
-
-  //! Wrapper of std::ostream for output.
-  class GenericWriteStream {
-    public:
-      typedef char Ch;    //!< Character type. Only support char.
-
-      //! Constructor
-      /*!
-        \param os Output stream.
-        */
-      GenericWriteStream(std::ostream& os) : os_(os) {
-      }
-
-      void Put(char c) {
-        os_.put(c);
-      }
-
-      void PutN(char c, size_t n) {
-        for (size_t i = 0; i < n; ++i) {
-          Put(c);
+        //! Constructor.
+        /*!
+          \param is Input stream.
+          */
+        GenericReadStream(std::istream &is) : is_(&is) {
         }
-      }
 
-      void Flush() {
-        os_.flush();
-      }
 
-      size_t Tell() const {
-        return (int)os_.tellp();
-      }
+        Ch Peek() const {
+            if (is_->eof()) return '\0';
+            return static_cast<char>(is_->peek());
+        }
 
-      // Not implemented
-      char Peek() const    { RAPIDJSON_ASSERT(false); }
-      char Take()          { RAPIDJSON_ASSERT(false); }
-      char* PutBegin()     { RAPIDJSON_ASSERT(false); return 0; }
-      size_t PutEnd(char*) { RAPIDJSON_ASSERT(false); return 0; }
+        Ch Take() {
+            if (is_->eof()) return '\0';
+            return static_cast<char>(is_->get());
+        }
+
+        size_t Tell() const {
+            return (int) is_->tellg();
+        }
+
+        // Not implemented
+        void Put(Ch) { RAPIDJSON_ASSERT(false); }
+
+        void Flush() { RAPIDJSON_ASSERT(false); }
+
+        Ch *PutBegin() {
+            RAPIDJSON_ASSERT(false);
+            return 0;
+        }
+
+        size_t PutEnd(Ch *) {
+            RAPIDJSON_ASSERT(false);
+            return 0;
+        }
+
+        std::istream *is_;
+    };
+
+
+    //! Wrapper of std::ostream for output.
+    class GenericWriteStream {
+    public:
+        typedef char Ch;    //!< Character type. Only support char.
+
+        //! Constructor
+        /*!
+          \param os Output stream.
+          */
+        GenericWriteStream(std::ostream &os) : os_(os) {
+        }
+
+        void Put(char c) {
+            os_.put(c);
+        }
+
+        void PutN(char c, size_t n) {
+            for (size_t i = 0; i < n; ++i) {
+                Put(c);
+            }
+        }
+
+        void Flush() {
+            os_.flush();
+        }
+
+        size_t Tell() const {
+            return (int) os_.tellp();
+        }
+
+        // Not implemented
+        char Peek() const { RAPIDJSON_ASSERT(false); }
+
+        char Take() { RAPIDJSON_ASSERT(false); }
+
+        char *PutBegin() {
+            RAPIDJSON_ASSERT(false);
+            return 0;
+        }
+
+        size_t PutEnd(char *) {
+            RAPIDJSON_ASSERT(false);
+            return 0;
+        }
 
     private:
-      std::ostream& os_;
-  };
+        std::ostream &os_;
+    };
 
-  template<>
-    inline void PutN(GenericWriteStream& stream, char c, size_t n) {
-      stream.PutN(c, n);
+    template<>
+    inline void PutN(GenericWriteStream &stream, char c, size_t n) {
+        stream.PutN(c, n);
     }
 
 } // namespace rapidjson
 
 // On MSVC, restore warnings state
 #ifdef _MSC_VER
-    #pragma warning(pop)
+#pragma warning(pop)
 #endif
 #endif // RAPIDJSON_GENERICSTREAM_H_
--- serialization/cereal/external/rapidjson/internal/strfunc.h
+++ serialization/cereal/external/rapidjson/internal/strfunc.h
@@ -2,7 +2,7 @@
 #define RAPIDJSON_INTERNAL_STRFUNC_H_
 
 namespace rapidjson {
-namespace internal {
+    namespace internal {
 
 //! Custom strlen() which works on different character types.
 /*!	\tparam Ch Character type (e.g. char, wchar_t, short)
@@ -10,15 +10,15 @@ namespace internal {
 	\return Number of characters in the string. 
 	\note This has the same semantics as strlen(), the return value is not number of Unicode codepoints.
 */
-template <typename Ch>
-inline SizeType StrLen(const Ch* s) {
-	const Ch* p = s;
-	while (*p != '\0')
-		++p;
-	return SizeType(p - s);
-}
+        template<typename Ch>
+        inline SizeType StrLen(const Ch *s) {
+            const Ch *p = s;
+            while (*p != '\0')
+                ++p;
+            return SizeType(p - s);
+        }
 
-} // namespace internal
+    } // namespace internal
 } // namespace rapidjson
 
 #endif // RAPIDJSON_INTERNAL_STRFUNC_H_
